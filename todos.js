f2b413f6076f3e3629767bd4c53e708c5f94f296
Ubuntu <jordanbrennan@superman.superman-1k305uzv.g8.internal.cloudapp.net>
2015-10-17 00:27:39 +0000

    initial commit

---
+++ todos.js
@@ -0,0 +1,14 @@
+var todos = [
+    {
+      text: "Finish Challenge 4",
+      finished: true
+    },
+    {
+      text: "Celebrate the weekend",
+      finished: false
+    },
+    {
+      text: "Let the dogs out",
+      finished: false
+    }
+];
